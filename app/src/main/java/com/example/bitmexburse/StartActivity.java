9a8cecd1b214d6ef6f0c8223ca3d0994977cbd42
vladislavgroskov <vladusik.168@gmail.com>
2019-07-03 13:32:28 +0300

    commit 5

--- app/src/main/java/com/example/bitmexburse/StartActivity.java
+++ app/src/main/java/com/example/bitmexburse/StartActivity.java
@@ -22,43 +22,150 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class StartActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    //bitcoin const
+    String databit = "";
+    String dataParsedbit = "";
+    String singleParsedbit = "";
+    //usd const
+    String datausd = "";
+    String dataParsedusd = "";
+    String singleParsedusd = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        final TextView bitcoin = findViewById(R.id.bitcoin);
         //My code
         TextView name_of_burse = findViewById(R.id.name_of_burse);
+        final TextView usd = findViewById(R.id.usd);
         String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
         name_of_burse.setText("Биржа на "+mydate);
-        //get-request
-        new OkHttpClient.Builder()
-                .connectTimeout(15, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(15, TimeUnit.SECONDS)
-                .build();
+        //get-request_for_bitcoin
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
 
-        new Request.Builder()
-                .url("wss://www.bitmex.com/realtime/?param1=value1&param2=value2")
-                .get()
-                .header("header-1-name", "header-1-value")
-                .header("header-2-name", "header-2-value")
-                .build();
+                String url ="https://www.bitmex.com/api/v1/orderBook/L2?symbol=XBTUSD&depth=1";
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
 
-        //Ending code
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()){
+                            final String myResponse = response.body().string();
+                            StartActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    databit = databit + myResponse;
+
+                                    try {
+                                        JSONArray ja = new JSONArray(databit);
+                                        for (int i=0;i<ja.length()-1;i++) {
+                                            JSONObject jo = (JSONObject) ja.get(i);
+                                            singleParsedbit = jo.get("price")+"";
+                                            dataParsedbit = dataParsedbit + singleParsedbit+"$";
+                                        }
+                                    }
+                                        catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                    bitcoin.setText("");
+                                    bitcoin.setText(dataParsedbit);
+                                    }
+                                });
+                            }
+                    }
+                });
+                databit="";
+                dataParsedbit="";
+                singleParsedbit="";
+                //USDDDD
+
+
+                String urlusd ="https://currate.ru/api/?get=rates&pairs=USDRUB&key=27be19e51da9131149118f1cbb82bed2";
+                Request requestusd = new Request.Builder()
+                        .url(urlusd)
+                        .build();
+                client.newCall(requestusd).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response responseusd) throws IOException {
+                        if (responseusd.isSuccessful()){
+                            final String myResponseusd = responseusd.body().string();
+                            StartActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    datausd = datausd + myResponseusd;
+                                    datausd = "["+datausd+"]";
+                                    try {
+                                        JSONArray ja = new JSONArray(datausd);
+                                        for (int i=0;i<ja.length();i++) {
+                                            JSONObject jo = (JSONObject) ja.get(i);
+                                            JSONObject joo = (JSONObject) jo.get("data");
+                                            singleParsedusd = joo.get("USDRUB")+"";
+                                            dataParsedusd = dataParsedusd + singleParsedusd;
+                                        }
+                                    }
+                                    catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                    usd.setText("");
+                                    usd.setText(dataParsedusd);
+                                }
+                            });
+                        }
+                    }
+                });
+                datausd="";
+                dataParsedusd="";
+                singleParsedusd="";
+
+                
+
+            }
+        };
+        timer.schedule(timerTask,0,1000);
+        //Endinggetrequest_bitcoin
+        //https://currate.ru/api/?get=rates&pairs=USDRUB&key=92b43dfd78329b5554d0e5471b4b6d0c
+
+        //get-request-USDRUB
+
+
 
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
